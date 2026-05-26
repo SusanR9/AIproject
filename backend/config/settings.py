cdf80deef4a6556f3a7c18f4d24ebf0acc18d540
Susan <Susan@LAPTOP-H1VFL98T>
2026-05-26 16:18:48 +0530

    database correction

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -66,17 +66,18 @@ pymysql.__version__ = '2.2.1'
 pymysql.version_info = (2, 2, 1, 'final', 0)
 
 DATABASES = {
-   
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQLDATABASE'),
-        'USER': os.environ.get('MYSQLUSER'),
-        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
-        'HOST': os.environ.get('MYSQLHOST'),
-        'PORT': os.environ.get('MYSQLPORT'),
+        'NAME': 'railway',
+        'USER': 'root',
+        'PASSWORD': 'vUoEsIvGqmkzcDjCGEmlMsWzseVDPzcD',
+        'HOST': 'kodama.proxy.rlwy.net',
+        'PORT': '3306',
+        'OPTIONS': {
+            'connect_timeout': 120,
+    },
     }
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
