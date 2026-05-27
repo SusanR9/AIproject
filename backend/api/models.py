da51050a667f5ca02acd1085c907bc6842147ecb
Susan <Susan@LAPTOP-H1VFL98T>
2026-05-28 03:30:51 +0530

    Fixed api migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -8,7 +8,7 @@ class Registration(models.Model):
     age = models.IntegerField(null=True, blank=True)
     phno = models.CharField(max_length=12)
     address = models.CharField(max_length=255, null=True, blank=True)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     bloodgroup = models.CharField(max_length=50, null=True, blank=True)
     identity_proof = models.FileField(upload_to='identity_proofs/', null=True, blank=True)
     candidate_photo = models.FileField(upload_to='candidate_photos/', null=True, blank=True)
@@ -22,18 +22,11 @@ class Registration(models.Model):
 
 class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
-
-    registration = models.ForeignKey(
-        Registration,
-        on_delete=models.CASCADE,
-        null=True,
-        blank=True
-    )
-
+    registration = models.ForeignKey(Registration, on_delete=models.CASCADE)
     mode_of_payment = models.CharField(max_length=50, default="Razorpay")
     payment_status = models.CharField(max_length=20, default="PENDING")
     transaction_id = models.CharField(max_length=100, null=True, blank=True)
-    amount = models.IntegerField(default=0)
+    amount = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
