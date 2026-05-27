3e47c204c9f0a1cdc8cca2d3f152fa58e58b5a7d
Susan <Susan@LAPTOP-H1VFL98T>
2026-05-28 03:09:55 +0530

    reset migrations

--- backend/api/models.py
+++ backend/api/models.py
@@ -2,117 +2,39 @@ from django.db import models
 
 
 class Registration(models.Model):
-    registration_id = models.AutoField(
-        db_column='Registration_id',
-        primary_key=True
-    )
-
-    first_name = models.CharField(
-        db_column='FirstName',
-        max_length=100
-    )
-
-    last_name = models.CharField(
-        db_column='LastName',
-        max_length=100
-    )
-
-    age = models.IntegerField(
-        db_column='age',
-        null=True,
-        blank=True
-    )
-
-    phno = models.CharField(
-        db_column='Phno',
-        max_length=12
-    )
-
-    address = models.CharField(
-        db_column='Address',
-        max_length=255,
-        null=True,
-        blank=True
-    )
-
-    email = models.CharField(
-        db_column='Email',
-        max_length=100
-    )
-
-    bloodgroup = models.CharField(
-        db_column='Bloodgroup',
-        max_length=50,
-        null=True,
-        blank=True
-    )
-
-    identity_proof = models.FileField(
-        db_column='identity_proof',
-        upload_to='identity_proofs/',
-        max_length=255,
-        null=True,
-        blank=True
-    )
-
-    candidate_photo = models.FileField(
-        db_column='candidate_photo',
-        upload_to='candidate_photos/',
-        max_length=255,
-        null=True,
-        blank=True
-    )
-
-    created_at = models.DateTimeField(
-        db_column='Created_at',
-        auto_now_add=True
-    )
-
-    competition_name = models.CharField(
-        db_column='Competition_name',
-        max_length=100,
-        null=True,
-        blank=True
-    )
-
-    type_of_enroll = models.CharField(
-        db_column='Type_of_Enroll',
-        max_length=100,
-        null=True,
-        blank=True
-    )
+    registration_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    age = models.IntegerField(null=True, blank=True)
+    phno = models.CharField(max_length=12)
+    address = models.CharField(max_length=255, null=True, blank=True)
+    email = models.CharField(max_length=100)
+    bloodgroup = models.CharField(max_length=50, null=True, blank=True)
+    identity_proof = models.FileField(upload_to='identity_proofs/', null=True, blank=True)
+    candidate_photo = models.FileField(upload_to='candidate_photos/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    competition_name = models.CharField(max_length=100, null=True, blank=True)
+    type_of_enroll = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
-        db_table = 'registration'
+        db_table = 'REGISTRATION'
 
 
 class Payment(models.Model):
+    payment_id = models.AutoField(primary_key=True)
+
     registration = models.ForeignKey(
         Registration,
-        on_delete=models.CASCADE
-    )
-
-    mode_of_payment = models.CharField(
-        max_length=50,
-        default="Razorpay"
-    )
-
-    payment_status = models.CharField(
-        max_length=20,
-        default="PENDING"
-    )
-
-    transaction_id = models.CharField(
-        max_length=100,
+        on_delete=models.CASCADE,
         null=True,
         blank=True
     )
 
-    amount = models.IntegerField()
-
-    created_at = models.DateTimeField(
-        auto_now_add=True
-    )
+    mode_of_payment = models.CharField(max_length=50, default="Razorpay")
+    payment_status = models.CharField(max_length=20, default="PENDING")
+    transaction_id = models.CharField(max_length=100, null=True, blank=True)
+    amount = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        db_table = 'payment'
+        db_table = 'PAYMENT'
